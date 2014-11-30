78fbddc1d005536ff7adb55fd48e91827830cd52
Sean Soper <sean.soper@gmail.com>
2014-11-30 15:45:51 -0500

    minor

--- AccelerateExample/ViewController.m
+++ AccelerateExample/ViewController.m
@@ -22,7 +22,7 @@ BOOL static TimeInfoSucceeded;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         if(mach_timebase_info(&TimeInfo) == KERN_SUCCESS)
@@ -37,16 +37,16 @@ BOOL static TimeInfoSucceeded;
     // [self complexMatrix: 50 n: 70 p: 10 naive: YES];
     // [self complexMatrix: 500 n: 700 p: 10 naive: YES];
     // [self complexMatrix: 5000 n: 7000 p: 10 naive: YES];
-    
+
     /*
      * These functions take a long time to complete
      */
     // [self complexMatrix: 5000 n: 7000 p: 100 naive: YES]; // Takes 88 seconds
-    // [self complexMatrix: 5000 n: 7000 p: 1000 naive: YES]; // Still wasn't finished about 90 minutes
-    
+    // [self complexMatrix: 5000 n: 7000 p: 1000 naive: YES]; // Still wasn't finished after 90 minutes
+
     // Step 3
     // [self simpleAcceleratedMatrix];
-    
+
     // Step 4
     // [self complexMatrix: 5 n: 7 p: 10 naive: NO];
     // [self complexMatrix: 50 n: 70 p: 10 naive: NO];
@@ -70,10 +70,10 @@ BOOL static TimeInfoSucceeded;
     // |1 2 3|   |7   8|   |58   64|
     // |4 5 6| x |9  10| = |139 154|
     //           |11 12|
-    
+
     NSArray *arrayA = @[@[@1,@2,@3], @[@4,@5,@6]];
     NSArray *arrayB = @[@[@7,@8], @[@9,@10], @[@11,@12]];
-    
+
     [self naiveMatrixMultiplication: arrayA matrixB: arrayB showResult: YES logTitle: @"Simple matrix"];
 }
 
@@ -81,7 +81,7 @@ BOOL static TimeInfoSucceeded;
     float a[6] = {1, 2, 3, 4, 5, 6};
     float b[6] = {7, 8, 9, 10, 11, 12};
     float result[4];
-    
+
     vDSP_mmul(a, 1, b, 1, result, 1, 2, 2, 3);
 }
 
@@ -91,39 +91,39 @@ BOOL static TimeInfoSucceeded;
                      n:(NSUInteger) n   // cols in matrix B
                      p:(NSUInteger) p   // cols in matrix A and rows in matrix B
                  naive:(BOOL) naive {
-    
+
     uint64_t startTime = mach_absolute_time();
-    
+
     dispatch_group_t group = dispatch_group_create();
-    
+
     __block NSArray *arrayA;
     dispatch_group_enter(group);
     [self buildMatrix: m innerCount: p completionBlock:^(NSArray *matrix) {
         arrayA = [NSArray arrayWithArray: matrix];
         dispatch_group_leave(group);
     }];
-    
+
     __block NSArray *arrayB;
     dispatch_group_enter(group);
     [self buildMatrix: p innerCount: n completionBlock:^(NSArray *matrix) {
         arrayB = [NSArray arrayWithArray: matrix];
         dispatch_group_leave(group);
     }];
-    
+
     dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
     NSString *title = [NSString stringWithFormat: @"Complex matrices of %ldx%ld and %ldx%ld", m, n, n, p];
-    
+
     uint64_t endTime = mach_absolute_time();
-    
+
     if(TimeInfoSucceeded) {
         uint64_t nanos = ((endTime - startTime) * TimeInfo.numer) / TimeInfo.denom;
         CGFloat timeTook = (CGFloat)nanos / NSEC_PER_MSEC;
         NSLog(@"Done building %@ took %f seconds", [title substringFromIndex: 8], timeTook);
     }
-    
+
     if (naive)
         return [self naiveMatrixMultiplication: arrayA matrixB: arrayB showResult: NO logTitle: title];
-    
+
     [self acceleratedMatrixMultiplication: arrayA arrayB: arrayB m: m n: n p: p showResult: NO logTitle: title];
 }
 
@@ -142,29 +142,30 @@ BOOL static TimeInfoSucceeded;
                            matrixB:(NSArray *) matrixB
                         showResult:(BOOL) showResult
                           logTitle:(NSString *) logTitle {
-    
+
     uint64_t startTime = mach_absolute_time();
-    
+
     NSMutableArray *matrix = [NSMutableArray new];
-    
+
     for (int i=0; i<matrixA.count; i++) {
         NSArray *arrayA = matrixA[i];
-        
+
         for (int n=0; n<matrixA.count; n++) {
-            
+
             for (int j=0; j<arrayA.count; j++) {
                 NSArray *arrayB = matrixB[j];
-                
+
                 NSNumber *result = [NSNumber numberWithInteger: [arrayA[j] integerValue] * [arrayB[n] integerValue]];
-                
+
                 if (i == matrix.count) {
                     [matrix addObject: [NSMutableArray array]];
                 }
-                
+
                 NSMutableArray *aryA = matrix[i];
-                
-                //                NSLog(@"i%u, j%u, n%u, %ld x %ld = %ld", i, j, n, [arrayA[j] integerValue], [arrayB[n] integerValue], result.integerValue);
-                
+
+                // Uncomment for debugging but be aware that it can seriously slow down computations
+                //  NSLog(@"i%u, j%u, n%u, %ld x %ld = %ld", i, j, n, [arrayA[j] integerValue], [arrayB[n] integerValue], result.integerValue);
+
                 if (n < aryA.count) {
                     NSNumber *curr = aryA[n];
                     curr = [NSNumber numberWithInteger: curr.integerValue + result.integerValue];
@@ -172,22 +173,22 @@ BOOL static TimeInfoSucceeded;
                 } else {
                     [aryA insertObject: result atIndex: n];
                 }
-                
+
             }
         }
     }
-    
+
     uint64_t endTime = mach_absolute_time();
-    
+
     if(TimeInfoSucceeded) {
         uint64_t nanos = ((endTime - startTime) * TimeInfo.numer) / TimeInfo.denom;
         CGFloat timeTook = (CGFloat)nanos / NSEC_PER_MSEC;
         NSLog(@"%@ took %f seconds", logTitle, timeTook);
     }
-    
+
     if (!showResult)
         return;
-    
+
     for (int i=0; i<matrix.count; i++) {
         NSArray *arrayA = matrix[i];
         NSLog(@"| %@ |", [arrayA componentsJoinedByString: @", "]);
@@ -201,36 +202,36 @@ BOOL static TimeInfoSucceeded;
                                        p:(NSUInteger) p
                               showResult:(BOOL) showResult
                                 logTitle:(NSString *) logTitle {
-    
+
     NSArray *flatA = [self flattened: arrayA];
     float *matrixA = (float *)malloc(flatA.count * sizeof(float));
     for (int i=0; i<flatA.count; i++) {
         matrixA[i] = (float)[flatA[i] floatValue];
     }
-    
+
     NSArray *flatB = [self flattened: arrayB];
     float *matrixB = (float *)malloc(flatB.count * sizeof(float));
     for (int i=0; i<flatB.count; i++) {
         matrixB[i] = [flatB[i] floatValue];
     }
-    
+
     float *result = (float *)malloc(m*n*sizeof(float));
-    
+
     uint64_t startTime = mach_absolute_time();
-    
+
     vDSP_mmul(matrixA, 1, matrixB, 1, result, 1, m, n, p);
-    
+
     uint64_t endTime = mach_absolute_time();
-    
+
     if(TimeInfoSucceeded) {
         uint64_t nanos = ((endTime - startTime) * TimeInfo.numer) / TimeInfo.denom;
         CGFloat timeTook = (CGFloat)nanos / NSEC_PER_MSEC;
         NSLog(@"%@ took %f seconds", logTitle, timeTook);
     }
-    
+
     if (!showResult)
         return;
-    
+
     for (int i=0; i<m*n; i++) {
         NSLog(@"Value %f", result[i]);
     }
@@ -241,7 +242,7 @@ BOOL static TimeInfoSucceeded;
 - (void) buildMatrix:(NSUInteger) outerCount
           innerCount:(NSUInteger) innerCount
      completionBlock:(void (^)(NSArray *matrix)) completionBlock {
-    
+
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         NSMutableArray *matrix = [NSMutableArray array];
         @autoreleasepool {
@@ -250,11 +251,11 @@ BOOL static TimeInfoSucceeded;
                 for (int j=0; j < innerCount; j++) {
                     [inner addObject: @(arc4random_uniform(10000)+1)];
                 }
-                
+
                 [matrix addObject: [[NSArray alloc] initWithArray: inner]];
             }
         }
-        
+
         completionBlock([NSArray arrayWithArray: matrix]);
     });
 }
